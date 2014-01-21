6bff5f969071454fe34eaf793a654781e1cfe7ab
SeungyoupBaek <bsy8220@naver.com>
2014-01-21 11:30:16 +0900

    실습 완료했습니다.

--- SocialFramework/ViewController.m
+++ SocialFramework/ViewController.m
@@ -7,12 +7,36 @@
 //
 
 #import "ViewController.h"
-
+#import <Social/Social.h>
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+- (IBAction)showSocialComposer:(id)sender {
+    NSString *service = SLServiceTypeFacebook;
+    // 페이스북에 글 작성이 가능한가?
+    if ([SLComposeViewController isAvailableForServiceType:service]) {
+        // 페이스북에 글을 작성하기 위한 뷰 컨트롤러 생성.
+        SLComposeViewController *composer = [SLComposeViewController composeViewControllerForServiceType:service];
+        // 글 작성시 초기에 입력될 이미지와 글
+        UIImage *image = [UIImage imageNamed:@"image.png"];
+        [composer addImage:image];
+        
+        [composer setInitialText:@"소셜 프레임워크를 이용한 글 쓰기 테스트"];
+        
+        composer.completionHandler = ^(SLComposeViewControllerResult result){
+            if (SLComposeViewControllerResultDone == result) {
+                NSLog(@"글 작성 완료");
+            }else{
+                NSLog(@"글 작성 취소");
+            }
+        };
+        
+        // 모달로 표시
+        [self presentViewController:composer animated:YES completion:nil];
+    }
+}
 
 - (void)viewDidLoad
 {
